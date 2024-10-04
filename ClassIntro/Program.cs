ac5cd97021fa6b3f94bee8885611e869937423de
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-10-04 22:37:00 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Kurs kurs1 = new Kurs();
+kurs1.KursAdi = "C#";
+kurs1.Egitmen = "EnginDemiroğ";
+kurs1.IzlenmeOranı = 68;
+
+Kurs kurs2 = new Kurs();
+kurs2.KursAdi = "Java";
+kurs2.Egitmen = "Ali Veli";
+kurs2.IzlenmeOranı = 45;
+
+Kurs kurs3 = new Kurs();
+kurs3.KursAdi = "Python";
+kurs3.Egitmen = "Hasan Yılmaz";
+kurs3.IzlenmeOranı = 78;
+
+Kurs[] kurslar = { kurs1 ,kurs2,kurs3};
+
+foreach (var kurs in kurslar)
+{
+    Console.WriteLine("Kurs Adı: "+kurs.KursAdi);
+}
+
+class Kurs
+{
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int IzlenmeOranı { get; set; }
+}
+
+
---
+++ KampIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region Degiskenler
+double dolarDun = 45.1;
+double dolarBugun = 14.6;
+#endregion
+
+
+#region Diziler
+
+string kurs1 = "c# kursu";
+string kurs2 = "java Kursu";
+string kurs3 = "python";
+
+String[] kurslar2 = new string[2]; 
+
+String[] kurslar = {kurs1,kurs2,kurs3};
+
+for (int i = 0; i < kurslar.Length; i++)
+{
+    Console.WriteLine("Kurs :"+kurslar[i]);
+}
+
+#endregion
