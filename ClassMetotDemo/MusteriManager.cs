d4d754e948bc8ebf4db0d35e200d28c01d1ff841
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-10-07 17:43:40 +0300

    Devami

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri ID :"+musteri.Ad+" eklendi.");
+        }
+
+        public void Listeleme(Musteri[] musteri)
+        {
+            foreach(Musteri m in musteri)
+            {
+                Console.WriteLine("Musteri ID: "+m.Id);
+                Console.WriteLine("Musteri AD: "+m.Ad);
+                Console.WriteLine("Musteri SOYAD: "+m.Soyad);
+                Console.WriteLine("------------");
+            }
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri ID :" + musteri.Ad + " silindi.");
+        }
+        
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Oğuzhan";
+            musteri1.Soyad = "Çakmak";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ali";
+            musteri2.Soyad = "Yılmaz";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Berkan";
+            musteri3.Soyad = "Tokat";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Listeleme(musteriler);
+
+            musteriManager.Ekleme(musteri3);
+
+            musteriManager.Silme(musteri2);
+
+
+            Console.Read();
+
+
+        }
+    }
+}
