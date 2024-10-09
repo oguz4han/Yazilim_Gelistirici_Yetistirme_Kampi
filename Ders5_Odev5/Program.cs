7040a4d4de4b26c8f5f6dc2be0f7449a4c23ac15
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-10-09 17:33:10 +0300

    Ders5_Odev4 devamı

---
+++ Ders5_Odev5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders5_Odev5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
